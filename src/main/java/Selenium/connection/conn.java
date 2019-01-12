7de01857b90bf24d015a686ab37ed855b6322fc5
dpatri <deepa.patri.ctr@telesishq.com>
2019-01-12 14:31:35 -0500

    aa

--- src/main/java/Selenium/connection/conn.java
+++ src/main/java/Selenium/connection/conn.java
@@ -3,13 +3,18 @@ package Selenium.connection;
 import org.testng.util.Strings;
 
 public class conn {
-    public static void main(Strings[] args){
+    public static void main(Strings[] args) {
 
-    }
     /*
     Connection con = DriverManager.getConnection(dbUrl,username,password);
     Class.forName("com.mysql.jdbc.Driver");
     Statement stmt = con.createStatement();
     stmt.executeQuery(select *  from employee;);
+    String getString()
+    int getInt()
+    double getDouble()
+    absolute (int rowNumber)
+
     * */
+    }
 }
