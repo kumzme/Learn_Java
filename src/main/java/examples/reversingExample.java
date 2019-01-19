41896c07775002ea48cd6b8b7ec550e5710f1a86
dpatri <deepa.patri.ctr@telesishq.com>
2019-01-19 13:34:58 -0500

    adding find duplicates using Set

---
+++ src/main/java/examples/reversingExample.java
@@ -0,0 +1,23 @@
+package examples;
+
+/*Reverse word by word  in a String.
+ with a space
+*/
+public class reversingExample {
+
+
+    public static void main(String[] args){
+
+        String input = " The Sky is blue ";
+        reverseWords(input);
+
+    }
+    public static void reverseWords(String str){
+        String[] aa = str.trim().split(" ");
+        String revserString = " ";
+        for(int i = aa.length-1; i>= 0 ; i--){
+           revserString = revserString + aa[i] + " ";
+        }
+        System.out.println(revserString);
+    }
+}
